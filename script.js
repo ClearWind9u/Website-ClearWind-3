48e2911f1e5665b583cd1ab9e49174f96e13e4f2
Glitch (glitch-hello-website) <none>
2024-03-20 16:10:42 +0000

    🍼🌈 Checkpoint ./script.js:65462025/1088

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const item = [
-  { name: 'Pizza 001', price: 5.9, quantity: 1 },
-  { name: 'Pizza 002', price: 6.9, quantity: 1 },
-  { name: 'Pizza 003', price: 7.9, quantity: 1 },
+  { name: "Pizza 001", price: 5.9, quantity: 1 },
+  { name: "Pizza 002", price: 6.9, quantity: 1 },
+  { name: "Pizza 003", price: 7.9, quantity: 1 },
 ];
 const SHIPPING = 2;
 
@@ -12,9 +12,9 @@ function render() {
   });
   const total = subTotal + SHIPPING;
 
-  $('#sub-total').innerText = "$${subTotal.toFixed(2)}";
-  $('#shipping').innerText = "$${SHIPPING}";
-  $('#total').innerText = "$${total.toFixed(2)}";
+  $("#sub-total").innerText = "$${subTotal.toFixed(2)}";
+  $("#shipping").innerText = "$${SHIPPING}";
+  $("#total").innerText = "$${total.toFixed(2)}";
 }
 function add() {}
 render();
